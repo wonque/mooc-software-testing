56fd09b8cee71191622430a74d96b1bddb73027b
Anatolijs Tolstihs <a.tolstihs@gmail.com>
2020-09-24 17:29:28 +0200

    Ex 1 - NumFinder

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -5,21 +5,24 @@ public class NumFinder {
     private int largest = Integer.MIN_VALUE;
 
     public void find(int[] nums) {
-        for(int n : nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("Argument should not be null");
+        }
+        for (int n : nums) {
 
-            if(n < smallest)
+            if (n < smallest)
                 smallest = n;
-            else if (n > largest)
+            if (n > largest)
                 largest = n;
 
         }
     }
 
-    public int getSmallest () {
+    public int getSmallest() {
         return smallest;
     }
 
-    public int getLargest () {
+    public int getLargest() {
         return largest;
     }
 }
