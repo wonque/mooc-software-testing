2517733c856d5c180df42229696237b156cc3428
Anatolijs Tolstihs <a.tolstihs@gmail.com>
2020-09-30 11:07:32 +0200

    practical ex 2

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -12,7 +12,6 @@ public class CaesarShiftCipher {
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
            
-            sb.append(currentChar);
             if (currentChar > 'z' || currentChar < 'a') {
                 return "invalid";
             } else if ((char) (currentChar + shift) > 'z') {
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,48 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CaesarShiftCipherTest {
+
+    private final CaesarShiftCipher victim = new CaesarShiftCipher();
+
+    @Test
+    public void testShiftNullMessage() {
+        assertThrows(NullPointerException.class, () -> victim.process(null, 4));
+    }
+
+    @Test
+    public void testShiftMixedCaseLetters() {
+        assertEquals("invalid", victim.process("MeSSSaGe", 3));
+    }
+
+    @Test
+    public void testShiftLettersIfShiftIsZero() {
+        assertEquals("abc", victim.process("abc", 0));
+    }
+
+    @Test
+    public void testNegativeShift() {
+        assertEquals("xyz", victim.process("abc", -3));
+    }
+
+    @Test
+    public void testPositiveShift() {
+        assertEquals("def", victim.process("abc", 3));
+        assertEquals("abc", victim.process("xyz", 3));
+    }
+
+    @Test
+    public void testWhitespaceString() {
+        assertEquals("invalid", victim.process("   ", 3));
+    }
+
+    @Test
+    public void testStringContainingWhiteSpace() {
+        assertEquals("invalid", victim.process("a b c", 3));
+    }
 
 }
